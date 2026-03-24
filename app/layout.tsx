7b5570be1fa658cbfc9f1ad5327b0dbbe8983ce6
Aaron Tseng <aarontseng@diatpetrillo.rad.rutgers.edu>
2026-03-24 18:41:09 -0400

    sign in with clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
+import { ClerkProvider, SignInButton, SignUpButton, Show, UserButton } from '@clerk/nextjs'
 import './globals.css'
 
 const _geist = Geist({ subsets: ["latin"] });
@@ -20,8 +21,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-sans antialiased`}>
-        {children}
-        <Analytics />
+        <ClerkProvider>
+          <header className="flex justify-end items-center px-4 py-2">
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton />
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+          </header>
+          {children}
+          <Analytics />
+        </ClerkProvider>
       </body>
     </html>
   )
